985c7d1ba06e9eaa0f3619f9a7d21b10123cd443
Rokoucha <6058487+rokoucha@users.noreply.github.com>
2026-08-15 17:08:05 +0900

    Update dependencies

--- pkgs/cloudflare-ddns.nix
+++ pkgs/cloudflare-ddns.nix
@@ -4,7 +4,7 @@
 }:
 let
   # renovate: datasource=github-tags depName=rokoucha/cloudflare-ddns versioning=regex:^(?<major>\d{8})\.(?<minor>\d{6})$
-  version = "20260527.153424";
+  version = "20260814.071357";
 in
 buildGoModule {
   pname = "cloudflare-ddns";
@@ -14,8 +14,8 @@ buildGoModule {
     owner = "rokoucha";
     repo = "cloudflare-ddns";
     rev = version;
-    hash = "sha256-Rft6hJy5Qs7Kqlc+zWFryHw+mxmdcRS9lvbzAR1e98o=";
+    hash = "sha256-Cp+OurtOY5v+GKDuSVwfqsrz5hp83upuooQzYYVCE1M=";
   };
 
-  vendorHash = "sha256-H6ilB9CDq71oPQzyVWm/fQbKlsHG+wyQPQ5HOHftXlU=";
+  vendorHash = "sha256-/8g3nt4SbSgDXe0lznZNJ6YDGhfpKmXqB27w6C407nM=";
 }
--- pkgs/networkd-prefix-watcher.nix
+++ pkgs/networkd-prefix-watcher.nix
@@ -4,7 +4,7 @@
 }:
 let
   # renovate: datasource=github-tags depName=rokoucha/networkd-prefix-watcher versioning=regex:^(?<major>\d{8})\.(?<minor>\d{6})$
-  version = "20260612.145210";
+  version = "20260721.143524";
 in
 buildGoModule {
   pname = "networkd-prefix-watcher";
@@ -14,7 +14,7 @@ buildGoModule {
     owner = "rokoucha";
     repo = "networkd-prefix-watcher";
     rev = version;
-    hash = "sha256-QEKjuX4QLsAINc/j2zzjbgrpCAdURJI2tw+BrrfG2Zc=";
+    hash = "sha256-l9ZacEttm0J9qkrAYNs0GC9J+vH4iKmyGRe5+TSWFtY=";
   };
 
   vendorHash = "sha256-7K17JaXFsjf163g5PXCb5ng2gYdotnZ2IDKk8KFjNj0=";
